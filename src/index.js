21e0cac3b17c98cc3cb8eef079668f11eb03ea02
VolhaP <polkhovskayaolga@mail.ru>
2019-10-13 21:04:56 +0300

    feat: 13 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let testStr = '';
+    
+    if (str.length % 2 !== 0) return false;
+    
+    let bracketsString = bracketsConfig.join().replace(/,/g, '');
+
+    nextBracket: for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsString.length; j++) {
+            if (str[i] == bracketsString[j]) {
+                testStr += str[i];
+                continue nextBracket;
+            }
+        }
+    }
+     return str === testStr ? true : false;  
 }
